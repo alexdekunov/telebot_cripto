1eea0c4541e80b5639226bf5b6ab218cbe38c24a
alexdekunov <wresda@gmail.com>
2023-10-28 15:10:16 +0300

    2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import telebot
 
 from datetime import datetime
-from settings import TOKEN_BOT, API_URL
+from settings import TOKEN, API_URL
 
 
 def get_data():
@@ -13,12 +13,13 @@ def get_data():
     print(f"{datetime.now().strftime('%Y-%m-%d %H:%M')}\nSell BTC price: {sell_price}")
 
 
-def telegram_bot(TOKEN_BOT):
-    bot = telebot.TeleBot(TOKEN_BOT)
+def telegram_bot(TOKEN):
+    bot = telebot.TeleBot(TOKEN)
 
     @bot.message_handler(commands=["start"])
     def start_message(message):
-        bot.send_message(message.chat.id, "Привет, напиши 'цена' что бы увидеть цену BTC.")
+        username = message.from_user.username
+        bot.send_message(message.chat.id, f"Привет @{username}, напиши 'цена' и я пришлю тебе цену BTC.")
 
     @bot.message_handler(content_types=["text"])
     def send_text(message):
@@ -40,17 +41,14 @@ def telegram_bot(TOKEN_BOT):
         else:
             bot.send_message(message.chat.id, "Проверь введённый текст!")
 
-
     bot.polling()
-
 
 
 if __name__ == '__main__':
     # get_data()
-    telegram_bot(TOKEN_BOT)
+    telegram_bot(TOKEN)
 
 # def send_message():
 #     print(API_KEY, DB_PASSWORD)
 #
 # send_message()
-
--- settings.py
+++ settings.py
@@ -5,6 +5,6 @@ dotenv.load_dotenv('.env')
 
 API_KEY = os.environ['API_KEY']
 DB_PASSWORD = os.environ['DB_PASSWORD']
-TOKEN_BOT = os.environ['TOKEN_BOT']
+TOKEN = os.environ['TOKEN']
 API_URL = os.environ['API_URL']
 
